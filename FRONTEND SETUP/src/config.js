5c954bec3e844ddfb93f9473f332cc2b431b1011
Vadlamudi Puneeth <nanipuneeth00@gmail.com>
2025-10-01 09:37:29 +0530

    frontend url changed for hosting

--- FRONTEND SETUP/src/config.js
+++ FRONTEND SETUP/src/config.js
@@ -1,4 +1,4 @@
 // Default configuration for local development
 export const config = {
-  BACKEND_URL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3333/api/v1"
+  BACKEND_URL: import.meta.env.VITE_BACKEND_URL || "https://my-shopping-app-n6pv.onrender.com"
 };
